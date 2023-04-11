2aae5baa66b02ba61d01ea45693370b2ce34fc4e
emreharman <emrehrmnb@gmail.com>
2023-04-11 23:22:51 +0300

    input component created

--- App.js
+++ App.js
@@ -5,34 +5,74 @@
  * @format
  */
 
-import React from 'react';
+import React, {useState} from 'react';
 import {
   SafeAreaView,
   ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
+  Alert,
 } from 'react-native';
 
 import Header from './src/components/header';
+import Input from './src/components/input';
 
 import generalStyles from './src/utils/generalStyles';
+import { colors } from './src/utils/constants';
 
 function App() {
+  const [text, setText] = useState('');
+  const [todos, setTodos] = useState([]);
+  const addTodo=()=>{
+    const newTodo={
+      id: String(new Date().getTime()),
+      text: text,
+      date: new Date(),
+      completed: false
+    }
+    setTodos([...todos, newTodo])
+    setText("")
+  }
   return (
-    <SafeAreaView style={generalStyles.flex1}>
-      <Header title='My Todo App' />
-      <View>
-        <Text>Todo App week1</Text>
+    <SafeAreaView style={[generalStyles.flex1,generalStyles.bgWhite]}>
+      <Header title="My Todo App" />
+      <Input
+        value={text}
+        onChangeText={text => setText(text)}
+        placeholder="deneme"
+        hasIcon
+        onIconPress={addTodo}
+      />
+      <View style={styles.todosWrapper}>
+        {todos.length === 0 ? (
+          <Text style={styles.emptyText}>Henüz Kayıtlı Bir Todo Bulunmamaktadır</Text>
+        ) : (
+          <ScrollView style={styles.scrollView}>
+            <Text>todo dizisi dolu</Text>
+          </ScrollView>
+        )}
       </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  
+  todosWrapper: {
+    flex:1,
+    marginHorizontal: 20,
+    marginVertical: 30,
+    borderWidth: 1
+  },
+  emptyText:{
+    textAlign:'center',
+    fontSize:20,
+    fontWeight:'bold',
+    color:colors.textPrimary
+  },
+  scrollView: {
+    flexGrow: 1
+  }
 });
 
 export default App;
